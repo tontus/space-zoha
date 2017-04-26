cc83ce8ced44d7d56ace340fa0b0055b0577620e
tonmoy <tonmoy.hexit@gmail.com>
2017-04-26 14:06:17 +0600

    initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject Zoha;
+	private Vector3 offset;
+	void Start () {
+		offset = transform.position - Zoha.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = Zoha.transform.position + offset;
+	}
+}
---
+++ Assets/Scripts/ZohaController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ZohaController : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+	public float speed;
+	private int count;
+	// public Text countText;
+	// public Text winText;
+	
+	void Start(){
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		SetCountText ();
+		// winText.text = "";
+		// speed = 10.0f;
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+		
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		// float moveHorizontal = Input.acceleration.x;
+		// float moveVertical = Input.acceleration.y;
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+	}
+	/// <summary>
+	/// Update is called every frame, if the MonoBehaviour is enabled.
+	/// </summary>
+	void Update()
+	{
+		 transform.up = rb2d.velocity;
+		// transform.rotation = Quaternion.LookRotation(rb2d.velocity);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			Destroy(other.gameObject);
+			count++;
+			SetCountText ();
+		} 
+		if (other.gameObject.CompareTag ("Hole")) {
+			
+			// StartCoroutine(Example());
+			
+			gameObject.SetActive (false);
+		
+			
+			if (count >= 11){
+				// winText.color = Color.blue;
+				// winText.text = "You Win!!";
+				StoreHighscore(count);
+
+			}
+				
+			else{
+				// winText.color = Color.red;
+				// winText.text = "Game Over";
+				StoreHighscore(count);
+			}
+				
+			
+			
+		}
+	}
+
+	 void StoreHighscore(int newHighscore){
+		int oldHighscore = PlayerPrefs.GetInt("highscore", 0);    
+		if(newHighscore > oldHighscore)
+			PlayerPrefs.SetInt("highscore", newHighscore);
+ 	}
+
+	void SetCountText(){
+		// countText.text = "Count " + count.ToString ();
+//		if (count >= 12) {
+//			winText.text = "You Win!!!"; 
+//		}
+	}
+}
