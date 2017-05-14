c583ac72559f0e14ddae71ef8a1c83686e862c00
tonmoy <tonmoy.hexit@gmail.com>
2017-05-14 15:13:50 +0600

    hole overlaping Most probably stopped

--- Assets/Scripts/RandomSpawn.cs
+++ Assets/Scripts/RandomSpawn.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class RandomSpawn : MonoBehaviour {
 
 	public GameObject[] collectables;
-	public GameObject blackHole;
+	
 	private int maxPickup;
 	private int minPickup;
-	private int holeCount;
+	
 	void Start () {
 		SpawnCoin();
 		
@@ -21,12 +21,11 @@ public class RandomSpawn : MonoBehaviour {
 	{
 		SpawnCoin();
 		DestroyCoin();
-		SpawnHole();
-		DestroyHole();
 
 	}
 
 	void SpawnCoin(){
+		int holeCount;
 		GameObject[] all;
 		all = GameObject.FindGameObjectsWithTag("Collectable");
 		int i = 0;
@@ -56,33 +55,6 @@ public class RandomSpawn : MonoBehaviour {
 		}
 	}
 	
-	void SpawnHole(){
-		GameObject[] all;
-		all = GameObject.FindGameObjectsWithTag("BlackHole");
-		int i = 0;
-		holeCount = 4 - all.Length;
-		while(i<holeCount){
-			int x = Random.Range(-50,50);
-			int y = Random.Range(-30,30);
-			while(x > -37 && x < 37 && y > -18 && y < 18){
-				x = Random.Range(-50,50);
-				y = Random.Range(-30,30);
-			}
-			Instantiate(blackHole, new Vector3(transform.position.x + x, transform.position.y + y, 0), Quaternion.identity);
-			i++;
-		}
-	}
-	void DestroyHole(){
-		GameObject[] all;
-		all = GameObject.FindGameObjectsWithTag("BlackHole");
-		int i = 0;
-		while(i < all.Length){
-			Vector3 pos = all[i].transform.position;
-			if (Mathf.Abs(transform.position.x - pos.x) > 50 || Mathf.Abs(transform.position.y - pos.y) > 30){
-				Destroy(all[i]);
-			}
-			i++;
-		}
-	}
+	
 
 }
