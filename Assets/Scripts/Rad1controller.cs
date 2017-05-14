2b102f8006df1e3bdc126c644578a60eee908c72
tonmoy <tonmoy.hexit@gmail.com>
2017-05-14 14:21:20 +0600

    habijabi state

--- Assets/Scripts/Rad1controller.cs
+++ Assets/Scripts/Rad1controller.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rad1controller : MonoBehaviour {
+public class Rad1Controller : MonoBehaviour {
 
-	IEnumerator OnTriggerEnter2D(Collider2D other)
-	{
-		if(other.gameObject.CompareTag ("Collectable") || other.gameObject.CompareTag("Obstacle")){
-			Destroy(other.gameObject);
+		// void OnTriggerEnter2D(Collider2D other)
+		// {
+			
+		// 	if(other.gameObject.CompareTag("BlackHole")){
+		// 		Debug.Log("a");
+		// 		Destroy(this.transform.parent.gameObject);
+		// 	}
+		// }
+		/// <summary>
+		/// Sent when an incoming collider makes contact with this object's
+		/// collider (2D physics only).
+		/// </summary>
+		/// <param name="other">The Collision2D data associated with this collision.</param>
+		void OnCollisionEnter2D(Collision2D other)
+		{
+			if(other.gameObject.CompareTag("BlackHole")){
+				Debug.Log("a");
+				Destroy(this.transform.gameObject);
+			}
 		}
-		if(other.gameObject.CompareTag ("Player")){
-			yield return new WaitForSeconds(2);
-			Debug.Log("done");
-        // SceneManager.LoadScene("StartScene");
-		}
-	}
 }
--- Assets/Scripts/RandomSpawn.cs
+++ Assets/Scripts/RandomSpawn.cs
@@ -58,15 +58,34 @@ public class RandomSpawn : MonoBehaviour {
 		all = GameObject.FindGameObjectsWithTag("BlackHole");
 		int i = 0;
 		holeCount = 4 - all.Length;
-		while(i<holeCount){
+		Vector2[] positions = new Vector2[4];
+		for(i=0; i< all.Length;i++){
+			positions[i] = all[i].transform.position;
+		}
+		Debug.Log(all.Length);
+
+		bool tag;
+		bool[] flag = new bool[4];
+		for(i=0; i< all.Length;i++){
+			flag[i] = true;
+		}
+		while(i < holeCount){
 			int x = Random.Range(-50,50);
 			int y = Random.Range(-30,30);
-			while(x > -37 && x < 37 && y > -18 && y < 18){
-				x = Random.Range(-50,50);
-				y = Random.Range(-30,30);
-			}
-			Instantiate(blackHole, new Vector3(transform.position.x + x, transform.position.y + y, 0), Quaternion.identity);
-			i++;
+			int j = 0;
+			
+			for (j = 0; j < all.Length; j++)
+				if (Vector2.Distance(positions[j],new Vector2(transform.position.x +x, transform.position.y + y)) <30)
+					break;
+			
+			
+			if(j==all.Length)
+				{
+					
+				}
+					Instantiate(blackHole, new Vector3(transform.position.x + x, transform.position.y + y, 0), Quaternion.identity);
+					i++;
+			
 		}
 	}
 	void DestroyHole(){
